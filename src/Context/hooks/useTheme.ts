4172c107bb32f7dfca2b2ad6dae8c2368c3f412c
Seacrs <shemachris072@gmail.com>
2026-04-26 15:40:59 +0200

    fix: correct ThemeContext import path in useTheme hook

--- src/Context/hooks/useTheme.ts
+++ src/Context/hooks/useTheme.ts
@@ -1,11 +1,11 @@
 import { useContext } from "react";
-import { ThemeContext } from "./index";
+import { ThemeContext } from "../index";
 
 export const useTheme = () => {
     const context = useContext(ThemeContext);
 
     if(!context) throw new Error("useTheme must be used within ThemeProvider");
-
+    
     const { theme, toggleTheme } = context;
 
     return { theme, toggleTheme };
