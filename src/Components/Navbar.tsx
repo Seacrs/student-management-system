50164eccf7bdedaaf340ee9e7d43b9cd55a937e0
Seacrs <shemachris072@gmail.com>
2026-04-26 11:19:18 +0200

    style: update navbar layout and add title

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -12,7 +12,10 @@ function Navbar(){
 
     return (
         <div className="">
-            <div className="p-3 grid grid-cols-3">
+            <div className="px-8 p-3 grid grid-cols-3">
+                <div>
+                    <h1 className="text-2xl">smgt</h1>
+                </div>
                 <div className="rounded-4xl max-w-10 flex items-center justify-center p-2 hover:bg-gray-300 dark:hover:bg-gray-700">
                     <button onClick={toggleTheme} >
                         <img src={theme ? sun : moon} alt="" className="w-8" />
