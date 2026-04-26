6b986df587e933b24a2141e020258d9600e113a5
Seacrs <shemachris072@gmail.com>
2026-04-26 09:46:47 +0200

    feat: add light and dark theme support using context

---
+++ src/Context/ThemeProvider.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { ThemeContext } from "./index";
+
+export interface ThemeProviderProps{
+    children: React.ReactNode
+}
+
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
+    const [theme, setTheme] = useState<boolean>(()=>{
+        return localStorage.getItem("dark") === "true";
+    });
+
+    useEffect(()=>{
+        document.documentElement.classList.toggle("dark", theme);
+        document.body.classList.toggle("dark", theme);
+        localStorage.setItem("dark", String(theme));
+    }, [theme]);
+
+    const toggleTheme = () => setTheme(prev => !prev);
+
+    return (
+        <ThemeContext value={{theme, toggleTheme}}>
+            { children }
+        </ThemeContext>
+    )
+}
+
+export default ThemeProvider;
