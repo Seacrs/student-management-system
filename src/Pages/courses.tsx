60bb7b384ed15c636f0173e13c77fcb71d7d19b5
Seacrs <shemachris072@gmail.com>
2026-04-27 20:09:50 +0200

    feat: improve courses page layout and responsiveness

--- src/Pages/courses.tsx
+++ src/Pages/courses.tsx
@@ -3,13 +3,15 @@ import { courses } from "../data/course";
 
 function Courses(){
     return (
-        <div className="p-5">
+        <div className="min-h-[calc(100vh-64px)] bg-stone-100 dark:bg-stone-950  p-6">
             <h1 className="text-xl font-bold">Courses</h1>
-                <div className="flex gap-3 p-5">
+                <div className="flex flex-col md:flex-row gap-3 p-5">
                     {courses.map(course => (
                         <Link key={course.title} to={`${course.title}`}>
-                            <div className="p-3 rounded-xl bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors">
-                                <img src={course.img} alt="" className="w-full h-64 object-cover rounded-2xl"/>
+                            <div className="p-3 rounded-xl bg-gray-200 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors">
+                                <div className="w-full h-52 overflow-hidden rounded-2xl">
+                                    <img src={course.img} alt="" className="w-full h-full object-cover"/>
+                                </div>
                                 <p className="mt-2 font-bold text-sm">{course.title}</p>
                             </div>
                         </Link>
